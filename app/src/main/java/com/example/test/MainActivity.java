c15b096de200837e21a3417468179503867bf473
geekerOne <111113800+geekerOne@users.noreply.github.com>
2022-08-13 03:05:49 +0430

    Update MainActivity.java

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -10,12 +10,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        Toast toastMessage;//for showing message
-        toastMessage = Toast.makeText(MainActivity.this, "Hello World!",
-        Toast.LENGTH_LONG);
-        toastMessage.show();
-        
     }
 
 }
